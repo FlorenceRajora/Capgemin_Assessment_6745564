ffefac280b7ecb1bd3076dbb29f55d0f74ab84fd
Florence <harshitarajora15@gmail.com>
2026-03-12 21:39:59 +0530

    DAY1

--- 28thFebAssessment/tests/Day11_Task3.spec.ts
+++ 28thFebAssessment/tests/Day11_Task3.spec.ts
@@ -2,12 +2,12 @@ import {test} from "@playwright/test"
 
 test("task3",async({page})=>{
     await page.goto("https://demoapps.qspiders.com/ui?scenario=1");
-    await page.locator('//input[@id="name"]').fill("AdityaTomar ");
-    await page.locator('//input[@id="email"]').fill("tomaraditya948@gmail.com");
-    await page.locator('//input[@id="password"]').fill("Adi@12u");
+    await page.locator('//input[@id="name"]').fill("Florence ");
+    await page.locator('//input[@id="email"]').fill("florence@gmail.com");
+    await page.locator('//input[@id="password"]').fill("Flora@15");
     await page.locator('button[type="submit"]').click();
-    await page.locator('//input[@id="email"]').fill("tomaraditya@gmail.com");
-    await page.locator('//input[@id="password"]').fill("Adi@12u");
+    await page.locator('//input[@id="email"]').fill("florence@gmail.com");
+    await page.locator('//input[@id="password"]').fill("Flora@15");
         await page.locator('button[type="submit"]').click();
         await page.screenshot({ path:'screenshot/task3.png'});
 
